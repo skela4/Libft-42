ae98ab01375f8a7fe22a835198c2a0b13e6f33fe
Aleksander AHIZI-ELLIAM <aleksander.ahizi@gmail.com>
2022-07-12 01:45:30 +0200

    :memo: Add documentation Chande variable name

--- ft_strlen.c
+++ ft_strlen.c
@@ -6,18 +6,30 @@
 /*   By: aahizi-e <aahizi-e@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/10 03:09:20 by aahizi-e          #+#    #+#             */
-/*   Updated: 2022/07/12 01:24:23 by aahizi-e         ###   ########.fr       */
+/*   Updated: 2022/07/12 01:44:19 by aahizi-e         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+/**
+* @file ft_strlen.c
+* @brief ft_strlen -- calculate the length of a string.
+* 
+* @see man 3 strlen
+*
+* @param const char *s
+*
+* @return 
+* 	The ft_strlen() function calculates the length of the string pointed to by
+*	s, excluding the terminating null byte ('\0')
+*/
 size_t	ft_strlen(const char *s)
 {
-	const char	*t;
+	const char	*end_ptr;
 
-	t = s;
-	while (*t)
-		++t;
-	return ((size_t)(t - s));
+	end_ptr = s;
+	while (*end_ptr)
+		++end_ptr;
+	return ((size_t)(end_ptr - s));
 }
