14ca5bdf995a03532d2df14f26194772fae7f726
Aleksander AHIZI-ELLIAM <aleksander.ahizi@gmail.com>
2022-07-13 03:32:33 +0200

    Fix norme 42 ternary forbidden

--- ft_toupper.c
+++ ft_toupper.c
@@ -6,7 +6,7 @@
 /*   By: aahizi-e <aahizi-e@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/09 01:23:17 by aleks             #+#    #+#             */
-/*   Updated: 2022/07/12 03:18:05 by aahizi-e         ###   ########.fr       */
+/*   Updated: 2022/07/13 03:31:59 by aahizi-e         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,9 @@
 * 	If the argument is a lowercase character, returns the uppercase version;
 *   otherwise, just returns the original argument.
 */
-int ft_toupper(int c)
+int	ft_toupper(int c)
 {
-    return ((c >= 'a' && c <= 'z') ? c - 32 : c);
+	if (c >= 'a' && c <= 'z')
+		c -= 32;
+	return (c);
 }
